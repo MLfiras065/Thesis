dd6cc2b86862c88c6143659b1b1d3852ee665ad7
fayrouzzahra <zbouzid75@gmail.com>
2024-06-23 13:10:28 +0100

    search product detail profil edit profile

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import OnBording from './Screens/OnBording';
 import Role from './Screens/Role';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,17 +7,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from './Screens/Login';
 import TopTabNav from './Screens/TopTabNav';
 import SignUp from './Screens/SignUp';
+import Search from "./Screens/Search"
+import React from 'react';
+import ProductDetails from './Screens/ProductDetails'; 
+import Profile from "./Screens/Profile"
+import EditProfile from "./Screens/EditProfile"
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
     <Stack.Navigator>
-    <Stack.Screen name="OnBording" component={OnBording}   options={{ headerShown: false }}/>
+    {/* <Stack.Screen name="OnBording" component={OnBording}   options={{ headerShown: false }}/>
    <Stack.Screen name="Role" component={Role} options={{ headerShown: false }} />
      <Stack.Screen name="TopTabNav" component={TopTabNav}  options={{ headerShown: false }}/>
     <Stack.Screen name="Login" component={Login} />  
-    <Stack.Screen name="SignUp" component={SignUp} />  
+    <Stack.Screen name="SignUp" component={SignUp} /> 
+    <Stack.Screen name="Search" component={Search} />  */}
+    <Stack.Screen name="ProductDetails" component={ProductDetails} /> 
+        {/* <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />  */}
   </Stack.Navigator>
     </NavigationContainer>
   );
