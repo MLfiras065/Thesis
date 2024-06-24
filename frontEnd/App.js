06ca02ecba061785f0b906033f7c544940b8dd0d
fayrouzzahra <zbouzid75@gmail.com>
2024-06-24 14:32:45 +0100

    check

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -5,7 +5,6 @@ import OnBording from './Screens/OnBording';
 import Role from './Screens/Role';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
 import Login from './Screens/Login';
 import TopTabNav from './Screens/TopTabNav';
 import BottomNavigation from './Screens/BottomNavigation.jsx';
@@ -13,7 +12,7 @@ import SignUp from './Screens/SignUp';
 import LogInUser from './Screens/ClientLogIn.jsx';
 import TopNav from './Screens/TopNav.jsx';
 import Subscribe from './Screens/Subscribe.jsx';
-// import Home from './Screens/Home.jsx';
+import Home from './Screens/Home.jsx';
 import Search from "./Screens/Search"
 import React from 'react';
 import ProductDetails from './Screens/ProductDetails'; 
@@ -22,30 +21,28 @@ import EditProfile from "./Screens/EditProfile"
 const Stack = createStackNavigator();
 export default function App() {
   return (
-   <Provider >
-    <NavigationContainer>
-    <Stack.Navigator>
-    {/* <Stack.Screen name="OnBording" component={OnBording}   options={{ headerShown: false }}/>
-    {/* <Stack.Screen name="Sub" component={Subscribe}   options={{ headerShown: false }}/> */}
-    <Stack.Screen name="Role" component={Role} options={{ headerShown: false }} />
-     <Stack.Screen name="TopTabNav" component={TopTabNav}  options={{ headerShown: false }}/>
-     <Stack.Screen name="TopNav" component={TopNav}  options={{ headerShown: false }}/>
+    <Provider >
+  <NavigationContainer>
+     <Stack.Navigator>
+    <Stack.Screen name="OnBording" component={OnBording}options={{ headerShown: false }}/>
+    <Stack.Screen name="Sub" component={Subscribe}options={{ headerShown: false }}/>
+    <Stack.Screen name="Role" component={Role} options={{headerShown: false }} />
+    <Stack.Screen name="TopTabNav" component={TopTabNav}options={{ headerShown: false }}/>
+    <Stack.Screen name="TopNav" component={TopNav}options={{ headerShown: false }}/>
     <Stack.Screen name="Login" component={Login} />  
     <Stack.Screen name="SignUp" component={SignUp} /> 
     <Stack.Screen name="LogIn" component={LogInUser} />  
     <Stack.Screen name="Navigation" component={BottomNavigation}  options={{headerShown:false}}/>
-    {/* <Stack.Screen name="Home" component={Home}  options={{headerShown:false}}/> */}
-    <Stack.Screen name="Search" component={Search} />  */}
+    <Stack.Screen name="Home" component={Home}  options={{headerShown:false}}/>
+    <Stack.Screen name="Search" component={Search} />  
     <Stack.Screen name="ProductDetails" component={ProductDetails} /> 
-        {/* <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="EditProfile" component={EditProfile} />  */}
-  </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Screen name="Profile" component={Profile} />
+    <Stack.Screen name="EditProfile" component={EditProfile} /> 
+    </Stack.Navigator>
+ </NavigationContainer>
    </Provider>
-    
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
