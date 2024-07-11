ffdb4edcea43897346afbd79d5cfdba2142968e5
fayrouzzahra <zbouzid75@gmail.com>
2024-07-11 08:58:20 +0100

    separation

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("thesis", "root", "0657firasML", {
+const sequelize = new Sequelize("thesis", "root", "root", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
