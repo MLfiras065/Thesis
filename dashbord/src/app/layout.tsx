195b6a93c654a6a73889cdefa1339dabe1d70c75
walid209 <159528753+walid209@users.noreply.github.com>
2024-07-11 09:49:39 +0200

    walido

--- dashbord/src/app/layout.tsx
+++ dashbord/src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Home from "./page";
-import { AuthProvider } from "../../../legacy/app/components/context/AuthContext";
-// import SignIn from "../app/SignIn/SignIn"
+
+// import { AuthProvider } from "../../../legacy/app/components/context/AuthContext";
+// // import SignIn from "../app/SignIn/SignIn"
 
 
 const inter = Inter({ subsets: ["latin"] });;
